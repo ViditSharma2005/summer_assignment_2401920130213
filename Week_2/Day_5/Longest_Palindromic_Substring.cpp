7d2223b1a83c5818aab17407ee510e603a47061b
Vidit Sharma <vidits852@gmail.com>
2026-06-13 00:35:11 +0530

    Longest Palindromic Substring

---
+++ Week_2/Day_5/Longest_Palindromic_Substring.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+#include<bits/stdc++.h>
+    string longestPalindrome(string s) {
+        int n= s.length();
+        if(n==0) return "";
+        string res="";
+        vector<int>range(2);
+        for(int i=0;i<n; i++){
+            //odd
+            int j=i-1, k=i+1;
+            while(j>=0 && k<n && s[j]==s[k]){
+                if((k-j)> range[1]-range[0]){
+                    range[0]=j, range[1]=k;
+                }
+                j--, k++;
+            }
+            
+
+            //even
+            j=i,k=i+1;
+            while(j>=0 && k<n && s[j]==s[k]){
+                if((k-j)> range[1]-range[0]){
+                    range[0]=j, range[1]=k;
+                }                
+                j--,k++;
+            }
+        }
+        for(int i=range[0]; i<=range[1] ; i++) res+=s[i];
+        return res;
+    }
+};
